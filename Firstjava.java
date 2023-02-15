9ae01347890c0c6a8260e2d7ee501d5a06ee202b
Sumangala K M <71866348+sumangalakm1@users.noreply.github.com>
2023-02-15 15:22:10 +0530

    Update Firstjava.java

--- Firstjava.java
+++ Firstjava.java
@@ -2,7 +2,7 @@ public class Firstjava
 {
 public static void main(String[] args)
 {
-System.out.println("Hello! Welcome to Jenkins!");
+System.out.println("Hello! Welcome to Jenkins! Demonstrating freestyle job");
 }
 }
 
